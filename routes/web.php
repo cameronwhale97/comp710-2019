e76448a9d9242388dd03eeffaa61dbcdd72aa62a
cameronwhale97 <cwhale@me.com>
2019-03-07 21:06:59 +0000

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::get('/posts', function () {
+    $name='Cameron'; 
+	return view('posts/welcome', compact('name'));
+});
+
+
+
+Route::get('posts/viewallposts', function () {
+	$posts=DB::table('tb_posts')->get();
+	return view('posts/viewallposts', compact('posts'));
+});
+
